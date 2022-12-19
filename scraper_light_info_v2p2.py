31df130ce408f46f1fb5b749fc2292c6e03d5603
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-19 15:27:09 +0100

    bug fix

--- scraper_light_info_v2p2.py
+++ scraper_light_info_v2p2.py
@@ -11,19 +11,19 @@ from selenium.webdriver.common.keys import Keys
 from url_parser import parse_url, get_url, get_base_url
 from datetime import date
 from datetime import timedelta
-from confing import target_townhouse,target_house,target_appt
+from config import target_townhouse,target_house,target_appt
 # V2.2 -> scrap data using filter to get more info
 
 # Application path
 application_path = "/Users/paul-emile/Documents/PythonProject/scraping/"
-file_name = 'real_estate_data_v2p2.csv'
+file_name = 'real_estate_data_v2p3.csv'
 file_path = os.path.join(application_path,file_name)
 target_links = [target_townhouse,target_house,target_appt]
 property_types = ['Row /Town House', 'House', 'Appartement']
 
 
 
-def scrape_page(df):
+def scrape_page(df,property_type):
 
 
     #get html container with their xpath
@@ -66,7 +66,7 @@ def scrape_page(df):
             posted = today - timedelta(days=float(8))
         else:
             time_number,time_unit,since = listed_since.split(' ')#depend the language
-            print(time_number,time_unit)
+
             if time_unit == 'min':
                 posted = today - timedelta(minutes=float(time_number))
             elif time_unit == 'hours':
@@ -75,7 +75,7 @@ def scrape_page(df):
                 posted = today - timedelta(days=float(time_number))
             else:# more than 7 days ago
                 posted = today- timedelta(days=float(8))
-            print(posted)
+
         try :#In case we don't have a bathroom
             bathrooms = container.find_element(by="xpath",value='./span/div/a/div/div/div[4]/div[2]/div/div').text
         except:
@@ -103,6 +103,7 @@ def scrape_page(df):
                                         'bedrooms':[bedrooms],
                                         'smaller_rooms':[smaller_rooms],
                                         'bathrooms':[bathrooms],
+                                        'property_type':[property_type],
                                         })
         #print(new_row)
         df = pd.concat([df,new_row],ignore_index=True)
@@ -121,7 +122,7 @@ def scrape_page(df):
     time.sleep(1)
     return df
 
-len_df = df.shape[0]
+
 
 
 def reset_first_page(driver):
@@ -215,12 +216,13 @@ def move_down(driver):
 def move(driver,LAT_MIN,LONG_MAX,LONG_MIN,moving_from_left_to_right):
 
     lat_min,lat_max,long_min,long_max = get_lat_long_min_max(driver)
-    if lat_max < LAT_MIN:
-        #end
+    if lat_max < LAT_MIN:#end
         return moving_from_left_to_right,False
+
     reset_first_page(driver)
+
     if moving_from_left_to_right:
-        print(long_max,LONG_MAX)
+
         if long_min<LONG_MAX:# we can still move right
             move_right(driver)
             return moving_from_left_to_right,True
@@ -229,8 +231,7 @@ def move(driver,LAT_MIN,LONG_MAX,LONG_MIN,moving_from_left_to_right):
             move_down(driver)
             return moving_from_left_to_right,True
     else:
-        print('switch')
-        print(long_max,LONG_MIN)
+
         if long_max>LONG_MIN:# we can still move right
             move_left(driver)
             return moving_from_left_to_right,True
@@ -239,17 +240,30 @@ def move(driver,LAT_MIN,LONG_MAX,LONG_MIN,moving_from_left_to_right):
             move_down(driver)
             return moving_from_left_to_right,True
 
+#chromedriver location
+path = "/Users/paul-emile/Downloads/chromedriver"
+
+# # Options
+# options = Options()
+# # don't open the browser
+# options.headless=True
+
+
+
+LONG_MIN = -79.92244 #left
+LONG_MAX = -79.05638  #right
+LAT_MIN = 43.61090
+
+today = date.today()
+# load or create a dataframe
+try:
+    df = pd.read_csv(file_path)
+except:
+    df = pd.DataFrame()
+
 for target_link,property_type in zip(target_links,property_types):
     #Target website
     website = target_link
-    #chromedriver location
-    path = "/Users/paul-emile/Downloads/chromedriver"
-
-    # # Options
-    # options = Options()
-    # # don't open the browser
-    # options.headless=True
-
     #Initializing Chrome driver
     service = Service(executable_path=path)
     driver = webdriver.Chrome(service = service)
@@ -262,31 +276,24 @@ for target_link,property_type in zip(target_links,property_types):
     time.sleep(20)
 
 
-    LONG_MIN = -79.92244 #left
-    LONG_MAX = -79.05638  #right
-    LAT_MIN = 43.61090
-    moving_from_left_to_right = True
-    today = date.today()
-    # load or create a dataframe
-    try:
-        df = pd.read_csv(file_path)
-    except:
-        df = pd.DataFrame()
 
     need_move = True
+    len_df = df.shape[0]
+    moving_from_left_to_right = True
     while need_move:
         max_page = get_max_page(driver)
 
         #print(max_page)
 
         for i in range(max_page):
-            df = scrape_page(df)
+            df = scrape_page(df,property_type)
 
         df.drop_duplicates(subset=['address'],inplace=True)
         moving_from_left_to_right,need_move = move(driver,LAT_MIN,LONG_MAX,LONG_MIN,moving_from_left_to_right)
-        time.sleep(4)#give the time to laod the page
+        time.sleep(4)#give time to load the page
 
     df.drop_duplicates(subset=['address'],inplace=True)
-    df['property_type'] = property_type
+
     df.to_csv(file_path,index=False)
+
     driver.quit()
